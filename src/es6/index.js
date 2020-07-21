7c3dc823809099e56386ada16797c3ac67e93553
Jorge Alberto Rodriguez Flores <jarf4321@gmail.com>
2020-07-21 01:07:26 -0500

    Agregados los ejemplos: var,let,const - destructuring - spread operator

--- src/es6/index.js
+++ src/es6/index.js
@@ -1,11 +1,12 @@
-// function newFuction(name, age, country) {
-//   var name = name || 'jorge'
-//   var age = age || 28
-//   var country = country || 'MX'
-//   console(name, age, country)
-// }
+/*-------------------------------------------------- Default params -----------------------------------------------------------------*/
+function newFuction(name, age, country) {
+  var name = name || 'jorge'
+  var age = age || 28
+  var country = country || 'MX'
+  console(name, age, country)
+}
 
-//*---------------- es6 --------------------*/
+/*---------------- es6 --------------------*/
 /* Ahora podemos agregar asi los parametros por defecto en caso de que no se le pasen parametros en las funciones */
 
 function newFunction2(name = 'jorge', age = 28, country = 'MX'){
@@ -15,10 +16,77 @@ function newFunction2(name = 'jorge', age = 28, country = 'MX'){
 newFunction2()
 newFunction2('Alberto', 30, 'CO')
 
+
+
+/*-------------------------------------------------- Template literals -------------------------------------------------------------*/
 let hello = "Hello"
 let world = "World"
 let epicPhrase = hello +" "+ world
 console.log(epicPhrase)
-// --- es6 - template literals
+
+/*---------------- es6 --------------------*/
+/* Usando comillas francesas podemos usar los template literals que se declaran asi ${variable}, con esto se quita la concatenacion de strings y se deja mas en claro el mensaje */
 let epicPhrase2 = `${hello} ${world}`
 console.log(epicPhrase2)
+
+/*------------------------------------- otro ejmeplo----- ---------------------*/
+let lorem = "Aqui deberia ir un lorem ipsum pero no tengo tiempo de buscarloooooooooooooooooooooooooooooooooooooooooooooooooooooooooo \n"
++ "otra frase epicaaaaaaaaaaaaaaaaa"
+
+/*---------------- es6 --------------------*/
+let lorem2 = `otra frase epica que necesitamos
+ahora es otra frase epica`
+
+console.log(lorem)
+console.log(lorem2)
+
+
+
+/*-------------------------------------------------- desctructuring -----------------------------------------------------------------*/
+let person = {
+  fullName: {
+    name: 'Jorge',
+    lastName: 'Rodriguez'
+  },
+  birthday: {
+    day: 'Tuesday',
+    month: 'February',
+    year: 1992
+  },
+  country: 'MX',
+  gender: 'Male'
+}
+
+console.log(person.fullName, person.birthday, person.country, person.gender)
+
+/*---------------- es6 - object destructuring --------------------*/
+
+let { fullName: { name } , birthday: { year:generation }, country, favoriteFood = 'pizza' } = person
+console.log( name, generation, country, favoriteFood)
+
+
+
+/*---------------------------------------------------- spread operator --------------------------------------------------------------*/
+let team1 = ['Oscar', 'Julian', 'Ricardo']
+let team2 = ['Valeria', 'Yesica', 'Camila']
+
+let education = ['David', ...team1, ...team2]
+
+console.log(education)
+
+
+
+/*-------------------------------------------------- var - let - const ------------------------------------------------------*/
+{
+  var globalVar = "Global var"
+}
+
+{
+  let globalLet = "Global Let"
+  console.log(globalLet)
+}
+
+console.log(globalVar)
+
+const a = 'b'
+a =  'a'
