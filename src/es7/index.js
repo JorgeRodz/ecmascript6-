4bac4c7f8cc644049865646ca1fa21edf2de6a7d
Jorge Alberto Rodriguez Flores <jarf4321@gmail.com>
2020-07-22 12:12:33 -0500

    Agregamos la carpeta es7 y creamos adentro de esta el archivo index.js que contiene las caracteristicas: includes y exponente

---
+++ src/es7/index.js
@@ -0,0 +1,18 @@
+/*-------------------------------------------------- includes ------------------------------------------------------*/
+/* Para saber si un elemento se encuentra dentro de otro elemento iterable */
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+
+if (numbers.includes(9)) {
+  console.log(`Si se encuentra el valor 7`)
+} else {
+  console.log(`No se encuentra`)
+}
+
+
+
+/*-------------------------------------------------- Elevar a la potencia -----------------------------------------------------------*/
+let base = 4
+let exponent = 4
+let result = base ** exponent
+
+console.log(result)
