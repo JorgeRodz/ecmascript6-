6eef8daf356e0c9bc9407000fb7aad3d61f392d4
Jorge Alberto Rodriguez Flores <jarf4321@gmail.com>
2020-07-22 11:53:55 -0500

    Se agregaron los ejemplos de es6: class, module and generators

--- src/es6/index.js
+++ src/es6/index.js
@@ -135,3 +135,45 @@ const helloPromise = () => {
 helloPromise()
 .then(resolveAnswer => console.log(resolveAnswer))
 .catch(error => console.log(error))
+
+
+
+/*-------------------------------------------------- Class ---------------- ------------------------------------------------------*/
+class calculator {
+  constructor() {
+    this.valueA = 0
+    this.valueB = 0
+  }
+  sum(valueA, valueB){
+    this.valueA = valueA
+    this.valueB = valueB
+    return this.valueA + this.valueB
+  }
+}
+
+const calc = new calculator()
+console.log(calc.sum(2, 2))
+
+
+
+/*--------------------------------------------------- modules -----------------------------------------------------------------------*/
+import { hello } from './module'
+
+hello()
+
+
+
+/*-------------------------------------------------- generators -------------------------------------------------------------------*/
+function* helloWorld() {
+  if (true) {
+    yield 'Hello, '
+  } 
+  if (true) {
+    yield 'World'
+  }
+}
+
+const generatorHello = helloWorld()
+console.log(generatorHello.next().value)
+console.log(generatorHello.next().value)
+console.log(generatorHello.next().value)
